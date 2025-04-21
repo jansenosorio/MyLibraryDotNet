69f309571300c01e7f49c19a0db147d9ab7fec6c
devosorio <contato.devosorio@gmail.com>
2025-04-21 19:06:46 -0300

    First Commit
    Add initial project setup and API controller
    
    - Update `.gitignore` to exclude temporary and user files.
    - Add `LibraryController` with a POST endpoint for book creation.
    - Modify `MyLibraryDotNet.csproj` to target .NET 8.0 and include Swagger.
    - Create `MyLibraryDotNet.http` for defining API requests.
    - Update or create solution file `MyLibraryDotNet.sln`.
    - Configure web application in `Program.cs` with necessary services.
    - Add `launchSettings.json` for different run profiles.
    - Create/update `appsettings.Development.json` and `appsettings.json` for logging.

---
+++ Controllers/LibraryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyLibraryDotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Create()
+        {
+            
+            return Ok("Book created!");
+        }
+
+    }
+}
