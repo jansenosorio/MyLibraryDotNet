f24c23dc775fc4324b6c5d6dc470563d74df2c8b
devosorio <contato.devosorio@gmail.com>
2025-04-21 21:07:59 -0300

    Add GetAll endpoint to LibraryController
    Implemented a new HTTP GET endpoint `GetAll` that returns a list of books. The method includes response type attributes for 200 OK and 400 Bad Request. It constructs a list of `CreateBookResponse` objects with sample book data.

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -25,5 +25,34 @@ namespace MyLibraryDotNet.Controllers
             return Created("", newBook);
         }
 
+        [HttpGet]
+        [ProducesResponseType(typeof(List<CreateBookResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetAll()
+        {
+            var books = new List<CreateBookResponse>
+            {
+                new() {
+                    Title = "Book 1",
+                    Author = "Author 1",
+                    Description = "Description 1",
+                    Genre = "Genre 1",
+                    Id = 1,
+                    Price = 10,
+                    QuantityAvailable = 5,
+                },
+                new() {
+                    Title = "Book 2",
+                    Author = "Author 2",
+                    Description = "Description 2",
+                    Genre = "Genre 2",
+                    Id = 2,
+                    Price = 20,
+                    QuantityAvailable = 3,
+                }
+            };
+
+            return Ok(books);
+        }
     }
 }
